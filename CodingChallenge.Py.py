783c93d61f8e89d87bae2ee2053d671e78aa685c
Matt Phair <nvmxp18@MattsMacbook.aholdusa.com>
2019-03-18 15:02:05 -0400

    Initial Commit

---
+++ CodingChallenge.Py.py
@@ -0,0 +1,39 @@
+""" WELCOME TO THE DIPLA CODING CHALLENGE!! """
+
+
+'''
+STEP 1:
+
+Read in both data files: transaction_information.csv and customer_preferences.json
+
+transaction information is tab delimited and customer preferences is record oriented
+'''
+
+
+
+'''
+STEP 2: 
+
+Merge the dataframes in to one master dataframe
+
+There are 500 customers in the transaction dataframe and 300 customers in the customer preference dataframe
+Only keep records which appear and both
+
+remove the objects that are no longer needed
+'''
+
+
+''' 
+STEP 3:
+
+Calculate total sales by month and plot them
+'''
+
+
+'''
+STEP 4:
+
+Use any model of your choice to understand the relationship between a person's sales and other metrics
+
+Make sure to use categorical and continuous variables
+'''
